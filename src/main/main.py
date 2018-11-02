53458bfa57c03433565e2bbdfe0ce67ecca780bd
changkaizhao <changkaizhao1006@gmail.com>
2018-11-03 00:01:37 +0800

    add tic tac toe game

--- src/main/main.py
+++ src/main/main.py
@@ -20,6 +20,7 @@ import tornado.ioloop
 import tornado.web
 from tornado.options import options, parse_config_file
 
+from main.handlers import MainHandler
 
 CONFIG_PATH = ''
 DEV_MODE = True
@@ -28,30 +29,6 @@ LOGGER_MODULE_NAME = '{0: <{width}}'.format('main', width=14)
 
 def normal_response(data):
     return {"error": None, "data": data}
-
-
-class MainHandler(tornado.web.RequestHandler):
-    """ 暂时用于测试上传图片 """
-
-    def get(self):
-        logger = logging.getLogger(LOGGER_MODULE_NAME)
-        logger.info("get Main request!!!")
-        self.write('''
-                    <html>
-                      <head><title>Upload File</title></head>
-                      <body>
-                        <form action='kousuan' enctype="multipart/form-data" method='post' target="sub_iframe">
-                        <input type='file' name='uploadfile'/><br/>
-                        <input type='submit' value='submit'/>
-                        </form>
-
-                        <iframe name="sub_iframe"></iframe>
-                      </body>
-                    </html>
-                    ''')
-
-
-
 
 def make_app():
     return tornado.web.Application([
